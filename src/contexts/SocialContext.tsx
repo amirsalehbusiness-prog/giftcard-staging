8c94087f12140c8e21e6a47ffd35353cbd6d1ec8
amirsalehbusiness-prog <amirsalehbusiness@gmail.com>
2025-08-15 02:14:04 +0330

    رفع خطای useEffect

--- src/contexts/SocialContext.tsx
+++ src/contexts/SocialContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, ReactNode } from 'react';
+import React, { createContext, useContext, ReactNode, useEffect } from 'react';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import type { 
   SocialProfile, 
